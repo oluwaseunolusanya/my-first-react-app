d2d8fc0d4fe0e88597c13b3fa0efb9c155f07865
oluwaseunolusanya <oluwaseun_olusanya@yahoo.com>
2023-09-02 18:00:29 +0100

    Created a first react app and updated 'App.js' to demonstrate react components, props and state.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+/* Person component to demonstrate react props
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  )
+}*/
+
+const App = () => {
+  /*const name = 'John'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person 
+        name={'John'}
+        lastName={'Doe'}
+        age={30}
+      />
+      <Person name={'Jane'}/>
+      <Person/>
     </div>
-  );
+    */
+   const [counter, setCounter] = useState(0);
+
+   useEffect(() => {
+    setCounter(100);
+   }, []);
+  
+   return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1) }>+</button>
+
+    </div>
+   );
 }
 
 export default App;
