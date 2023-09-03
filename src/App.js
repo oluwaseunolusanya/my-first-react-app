bb869fc437682bd47ef4750f6e1e4764732057bb
oluwaseunolusanya <oluwaseun_olusanya@yahoo.com>
2023-09-03 15:42:06 +0100

    Created a MovieCard component and searchIcon.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,71 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
 
-/* Person component to demonstrate react props
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  )
-}*/
+import MovieCard from "./MovieCard";
+
+import './App.css';
+import SearchIcon from './search.svg'
+
+//6414b6e4  - OMDb API Key
+
+const API_URL = 'https://www.omdbapi.com/?apikey=6414b6e4';
+
+const movie1 = {
+    "Title": "Miles Morales Ultimate Spiderman",
+    "Year": "2021",
+    "imdbID": "tt14311386",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNmMzODkwNDktMTkyMy00MmU5LWE4MGMtYzIzZjdjNmJiZDRiXkEyXkFqcGdeQXVyNDU1NDQ0NzE@._V1_SX300.jpg"
+  };
 
 const App = () => {
-  /*const name = 'John'
-  return (
-    <div className="App">
-      <Person 
-        name={'John'}
-        lastName={'Doe'}
-        age={30}
-      />
-      <Person name={'Jane'}/>
-      <Person/>
-    </div>
-    */
-   const [counter, setCounter] = useState(0);
+    const [movies, setMovies] = useState([]);
 
-   useEffect(() => {
-    setCounter(100);
-   }, []);
-  
-   return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1) }>+</button>
+    const [searchTerm, setSearchTerm] = useState('');
 
-    </div>
-   );
+    const searchMovies = async (title) => {
+        const response = await fetch (`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies('Kingsman');
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"                
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+
+        </div>
+    );
 }
 
 export default App;
